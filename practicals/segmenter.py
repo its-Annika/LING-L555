8e3f0c3cfe89ad3ed4ad3fb28db1aed6c5c9d9c5
Annika Shankwitz <annikashankwitz@Interstellar.local>
2023-09-07 18:56:21 -0400

    python stuff

---
+++ practicals/segmenter.py
@@ -0,0 +1,15 @@
+import sys
+ 
+#gets the orginal text
+originalText = sys.stdin.read()
+
+#removes the existing new line characters
+#because they aren't meaningful yet
+noNewLines = originalText.replace('\n', '')
+
+#finds the pattern '. ' 
+#and replaces it with '.\n'
+finalText = noNewLines.replace('. ', '.\n')
+
+#outputs the new version of the text
+sys.stdout.write(finalText)
