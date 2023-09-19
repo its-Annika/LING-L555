36c9c315baeb8e30083f8c6ba01c25dfa0a18a83
Annika Shankwitz <annikashankwitz@Interstellar.local>
2023-09-19 15:23:29 -0400

    done :)

--- practicals/tokeniser.py
+++ practicals/tokeniser.py
@@ -9,19 +9,19 @@ data = sys.stdin.read()
 # \n punctuation \n
 #this could be repeated until all the chosen punctuation
 #were accounted for
-a = data.replace(" ", '\n')
-b = a.replace('.', "\n.\n")
-c = b.replace(',', "\n,\n")
-d = c.replace('(', "\n(\n")
-e = d.replace(')', "\n)\n")
+#a = data.replace(" ", '\n')
+#b = a.replace('.', "\n.\n")
+#c = b.replace(',', "\n,\n")
+#d = c.replace('(', "\n(\n")
+#e = d.replace(')', "\n)\n")
 
 #this corrects having too many \n in a row
 #if one or more \n are found in a row, replace them with a single \n
 #which prevents empty lines in the output
-f = re.sub(r'\n+', '\n', e)
+#f = re.sub(r'\n+', '\n', e)
 
 #output it
-sys.stdout.write(f)
+#sys.stdout.write(f)
 
 ####################################################################################
 
@@ -30,7 +30,7 @@ sys.stdout.write(f)
 #seemed like more work than replacing everything in one process
 
 #storage
-#finalString = ""
+finalString = ""
 
 #for each character
 #if it's a space, turn add a \n to the finalString
@@ -38,16 +38,29 @@ sys.stdout.write(f)
 # \n punctuation \n to finalString
 # else, add the character as it is to finalString
 
-#for character in data:
-#	if character == " ":
-#		finalString += '\n'
-#	elif character in ",.():\"\'?!":
-#		finalString += '\n' +  character + '\n'
-#	else:
-#		finalString += character 
+for character in data:
+	if character == " ":
+		finalString += '\n'
+	elif character in ",.():\"\'?!":
+		finalString += '\n' +  character + '\n'
+	else:
+		finalString += character 
 
 #gets rid of duplicate \n characters
-#finalStringV2  = re.sub(r'\n+', '\n', finalString)
+finalStringV2  = re.sub(r'\n+', '\n', finalString)
 
-#output
-#sys.stdout.write(finalStringV2)
+#turns the lines into a list
+tokens = finalStringV2.split('\n')
+
+#finds the longest element for spacing reasons
+maxLen = len( max(tokens, key = len))
+
+#keeps track of the count
+count = 1
+
+#printing time!
+sys.stdout.write("ID\tLEMMA\tUPOS\tXPOS\tFLEATS\tHEAD\tDEPREL\tDEPS\tMISC")
+for token in tokens:
+	sys.stdout.write(str(count) + "\t" + token + "\t" + "\t-\t-\t-\t-\t-\t-\t-\t-\t\n")
+	count += 1
+
